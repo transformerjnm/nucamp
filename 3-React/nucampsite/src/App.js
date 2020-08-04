5b1a48734526462375e427632a5ec18f92bd541d
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-08-04 10:56:21 -0400

    added directory component and images.

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
 
 class App extends Component {
 
-  render( ) {
+    render() {
 
-      return (
+        return (
 
-          <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-          </div>
-          
-      );
+            <div className="App">
 
-  }
+                <Navbar dark color="primary">
+
+                <div className="container">
+
+                    <NavbarBrand href="/">NuCamp</NavbarBrand>
+
+                </div>
+
+                </Navbar>
+                <Directory />
+                
+            </div>
+
+        );
+
+    }
 
 }
 
