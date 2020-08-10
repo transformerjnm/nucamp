5b3fb7721d3cca3b8b5f32f628dc07c63bb7904d
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-08-10 13:05:00 -0400

    created a container main component.js
    app js now calls main containter js and main call all the components.

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,59 +1,57 @@
 import React, { Component, Fragment } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-    renderCampsite = (campsite) => {
+let RenderCampsite = ( campsite ) => {
+    return(
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                  <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+};
+
+let RenderComments = (comments) => {
+    if(comments){
         return(
             <div className="col-md-5 m-1">
-                <Card>
-
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-
-                    </CardBody>
-
-                </Card>
+                <h4>Comments</h4>
+                { 
+                    comments.map((comment) => {                        
+                        return(
+                            <Fragment key={comment.id}>
+                                <p>{comment.text}</p>
+                                <p>
+                                    {
+                                        comment.author},&nbsp;
+                                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))
+                                    }
+                                </p>
+                            </Fragment>                           
+                        );                      
+                    })
+                }
             </div>
         );
-    };
-    renderComments = (comments) => {
-        if(comments){
-            return(
-                <div className="col-md-5 m-1">
-                    <h4>Comments</h4>
-                    { 
-                        comments.map((comment) => {                        
-                            return(
-                                <Fragment key={comment.id}>
-                                    <p>{comment.text}</p>
-                                    <p>
-                                        {
-                                            comment.author},&nbsp;
-                                            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))
-                                        }
-                                    </p>
-                                </Fragment>                           
-                            );                      
-                        })
-                    }
-                </div>
-            );
-        }
-    };
-    render(){
-        if(this.props.campsite){
-            return(
-                <div className="row">{this.renderCampsite(this.props.campsite)} {this.renderComments(this.props.campsite.comments) }</div>               
-            );
-        } else {
-            return (
-                <div></div>
-            );
-        }
-    };
+    }
+};
+
+function CampsiteInfo(props) {
+    if(props.campsite){
+        return(
+            <div className="container">
+                <div className="row">{RenderCampsite(props.campsite)} {RenderComments(props.campsite.comments) }</div>
+            </div>               
+        );
+    } else {
+        return (
+            <div></div>
+        );
+    }
 };
 
 export default CampsiteInfo;
