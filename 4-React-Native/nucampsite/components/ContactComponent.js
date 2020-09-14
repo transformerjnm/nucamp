528f510047286f2b8c1288acbd497071888959c1
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-09-14 19:19:03 -0400

    react native week one

---
+++ 4-React-Native/nucampsite/components/ContactComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Contact extends Component {
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <Card title="Contact Information" wrapperStyle={{margin: 20}}>
+                    <Text>
+                        1 Nucamp Way{"\n"}
+                        Seattle, WA 98001{"\n"}
+                        U.S.A.{"\n"}
+                        {"\n"}
+                        Phone: 1-206-555-1234{"\n"}
+                        Email: campsites@nucamp.co{"\n"}
+                    </Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+}
+
+export default Contact;
