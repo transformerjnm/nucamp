85d7454aaf26e963401ba09d24e9db0d43c698ff
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-09-28 13:59:05 -0400

    secure store and local notifications

--- 4-React-Native/nucampsite/components/ReservationComponent.js
+++ 4-React-Native/nucampsite/components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import DatePicker from 'react-native-datepicker';
 import { Text, View, StyleSheet, Picker, Switch, Button,  Alert } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
     constructor(props) {
@@ -31,8 +33,11 @@ class Reservation extends Component {
                     onPress: () => this.resetForm()
                 },
                 {
-                    text: 'OK',
-                    onPress: () => this.resetForm()
+                    text: 'OK', 
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }
                 }
             ],
             { cancelable: false }
@@ -45,6 +50,28 @@ class Reservation extends Component {
             hikeIn: false,
             date: '',
         });
+    }
+
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
     }
 
     render() {
