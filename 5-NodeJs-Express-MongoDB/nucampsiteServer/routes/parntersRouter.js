57d18dc0bc3faa32a6aa2f7b3c927aa7dd574a68
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-10-19 19:02:08 -0400

    express generator

---
+++ 5-NodeJs-Express-MongoDB/nucampsiteServer/routes/parntersRouter.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const partnersRouter = express.Router();
+
+partnersRouter.use(bodyParser.json());
+
+partnersRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the partners to you');
+})
+.post((req, res) => {
+    res.end(`Will add the partners: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res) => {
+    res.end('Deleting all partners');
+});
+
+partnersRouter.route('/:partnersid')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send the partners to you');
+})
+.post((req, res) => {
+    res.end(`Will add the partners: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res) => {
+    res.end('Deleting all partners');
+});
+
+module.exports = partnersRouter;
