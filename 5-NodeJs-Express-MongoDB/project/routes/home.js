0de6a3a5db3e07b42adf9cbd8f011357e220b7c9
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-10-27 10:27:40 -0400

    testing

--- 5-NodeJs-Express-MongoDB/project/routes/home.js
+++ 5-NodeJs-Express-MongoDB/project/routes/home.js
@@ -9,29 +9,16 @@ homeRouter.use(bodyParser.json());
 homeRouter.route('/')
 .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/plain');
     next();
 })
 .get(async (req, res) => {
     try {
         const products = await Product.find();
-        res.json(products);
+        res.json(products[0]);
     } catch(err){
         res.json(err);
     }
-}).post((req, res) => {
-    const product = new Product({
-        id: req.body.id,
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price,
-        imgSrc: req.body.imgSrc,
-        imgAlt: req.body.imgAlt,
-        bestSeller: req.body.bestSeller
-    });
-    product.save()
-    .then( data => res.json(data))
-    .catch( err => console.log(err));
 })
 
 module.exports = homeRouter;
